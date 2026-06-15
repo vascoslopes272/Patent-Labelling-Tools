ba1fe87e7a1c5a4190bda745e8c14cd5b56c6846
vascoslopes272 <vasco.lopes@tecnico.ulisboa.pt>
2026-06-15 22:48:21 +0100

    d

--- src/figure_matcher.py
+++ src/figure_matcher.py
@@ -65,6 +65,7 @@ def build_engine(cfg: dict) -> tuple[Qwen2_5_VLForConditionalGeneration, AutoPro
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=True,
+        llm_int8_enable_fp32_cpu_offload=True,
     )
 
     model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
